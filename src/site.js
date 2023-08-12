fbb52493a1a02bc8478cafec820fd9ffa2c385f1
LaMarr Smith <81063441+lamarr-smith17@users.noreply.github.com>
2023-08-12 12:56:18 -0400

    started styling home

--- src/site.js
+++ src/site.js
@@ -29,31 +29,48 @@ function createMain()
 function createNavBar()
 {
     const navbar = document.createElement('nav');
+    navbar.id = "sideNavBar";
+    
     const homeButton = document.createElement('button');
     homeButton.textContent = "Home";
     homeButton.classList.add("button-styles");
-    homeButton.addEventListener("click", (e)=> {
+    homeButton.addEventListener("click", ()=> {
+        addActiveState(homeButton);
         Home();
     })
 
     const menuButton = document.createElement('button');
     menuButton.textContent = "Menu";
     menuButton.classList.add("button-styles");
-    menuButton.addEventListener("click", (e)=>{
-            Menu();
+    menuButton.addEventListener("click", ()=>{
+        addActiveState(menuButton);
+        Menu();
     })
 
     const contactButton = document.createElement('button');
     contactButton.textContent = "Contact Us";
     contactButton.classList.add("button-styles");
-    contactButton.addEventListener("click", (e)=>{
+    contactButton.addEventListener("click", ()=>{
+        addActiveState(contactButton);
         Contact();
     })
 
+    
     navbar.appendChild(homeButton);
     navbar.appendChild(menuButton);
     navbar.appendChild(contactButton);
     return navbar;
+}
+function addActiveState(button)
+{
+    const buttons = document.querySelectorAll(".button-styles");
+    buttons.forEach(button => {
+        if(button !== this)
+        {
+            button.classList.remove("active")
+        }
+    });;
+    button.classList.add("active");
 }
 function intializeHome()
 {
